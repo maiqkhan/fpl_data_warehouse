d6bf89744a186dbe59eaac8f23b2cc529b0c1445
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-11-07 00:03:27 -0500

    add pandas to dagster dev environment

--- fpl_project/setup.py
+++ fpl_project/setup.py
@@ -3,9 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="fpl_project",
     packages=find_packages(exclude=["fpl_project_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud"
-    ],
-    extras_require={"dev": ["dagster-webserver", "pytest"]},
+    install_requires=["dagster", "dagster-cloud"],
+    extras_require={"dev": ["dagster-webserver", "pytest", "pandas"]},
 )
