64eff80ec9f763f6d2a24bf8bfa99bfa870b1cfd
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-11-07 00:09:32 -0500

    add database connection function

---
+++ initial_load/database.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+server = os.getenv("SERVER")
+server_port = os.getenv("SERVER_PORT")
+database = os.getenv("DATABASE")
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+
+
+engine = create_engine(
+    f"postgresql+psycopg2://{username}:{password}@{server}:{server_port}/{database}"
+)
+
+# SessionalLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_conn():
+    return engine.connect()
