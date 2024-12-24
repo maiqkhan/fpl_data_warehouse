a412d640fe407b8993bb1e4c9233b32e52d4a3a5
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-24 16:55:45 -0500

    add schedules and jobs to definitions file

--- fpl_project/fpl_project/definitions.py
+++ fpl_project/fpl_project/definitions.py
@@ -1,12 +1,34 @@
-from dagster import Definitions, load_assets_from_modules, EnvVar
+from dagster import (
+    Definitions,
+    load_assets_from_modules,
+    EnvVar,
+    define_asset_job,
+    AssetSelection,
+    ScheduleDefinition,
+    schedule,
+    RunRequest,
+)
 
-from fpl_project.fpl_project.assets import assets
+from fpl_project.fpl_project.assets import players, raw, fixtures, teams
 from fpl_project.fpl_project.resources import postgres, fpl_api  # noqa: TID252
 
-all_assets = load_assets_from_modules([assets])
+all_assets = load_assets_from_modules([players, raw, fixtures, teams])
+all_assets_job = define_asset_job(
+    name="initial_job", selection=["*fixtures", "*players", "*teams"]
+)
+
+
+@schedule(job=all_assets_job, cron_schedule="*/5 * * * *")
+def test_schedule():
+    return RunRequest(
+        run_key=None,
+    )
+
 
 defs = Definitions(
     assets=all_assets,
+    jobs=[all_assets_job],
+    schedules=[test_schedule],
     resources={
         "fpl_server": postgres.PostgresResource(
             credentials=postgres.CredentialsResource(
