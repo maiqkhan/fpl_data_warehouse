d51e75034d9e394730aa62664b21910e04fd2a38
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-24 16:56:41 -0500

    add raw assets

---
+++ fpl_project/fpl_project/assets/raw.py
@@ -0,0 +1,49 @@
+from dagster import asset, AssetExecutionContext
+from fpl_project.fpl_project.resources.fpl_api import FplAPI
+from typing import Dict, List
+
+
+@asset(
+    group_name="RAW_DATA",
+    description="""Game data from FPL api bootstrap-static endpoint""",
+)
+def raw_bootstrap(fpl_api: FplAPI) -> Dict:
+
+    payload = fpl_api.get_request(endpoint="bootstrap-static/").json()
+
+    # raw_player_data_df = pd.DataFrame.from_records(payload)
+
+    return payload
+
+
+@asset(
+    group_name="RAW_DATA",
+    description="""Game data from FPL api bootstrap-static endpoint""",
+)
+def raw_players(context: AssetExecutionContext, raw_bootstrap: Dict) -> List[Dict]:
+
+    context.log.info(raw_bootstrap["elements"])
+
+    return raw_bootstrap["elements"]
+
+
+@asset(
+    group_name="RAW_DATA",
+    description="""Game data from FPL api bootstrap-static endpoint""",
+)
+def raw_teams(context: AssetExecutionContext, raw_bootstrap: Dict) -> List[Dict]:
+
+    context.log.info(raw_bootstrap["teams"])
+
+    return raw_bootstrap["teams"]
+
+
+@asset(
+    group_name="RAW_DATA",
+    description="""Game data from FPL api bootstrap-static endpoint""",
+)
+def raw_fixtures(context: AssetExecutionContext, fpl_api: FplAPI) -> List[Dict]:
+
+    payload = fpl_api.get_request(endpoint="fixtures/").json()
+
+    return payload
