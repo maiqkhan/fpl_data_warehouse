5ab98597dbfaaa7a1fadf0c650b6fc422d18ce20
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-26 13:04:05 -0500

    add match assets

---
+++ fpl_project/fpl_project/assets/matches.py
@@ -0,0 +1,73 @@
+from dagster import (
+    asset,
+    AssetExecutionContext,
+    asset_check,
+    AssetCheckResult,
+    AssetCheckExecutionContext,
+)
+from fpl_project.fpl_project.resources.fpl_api import FplAPI
+from typing import Dict, List
+import pandas as pd
+
+
+@asset(
+    group_name="MATCH",
+    description="""All match statistics for a given player""",
+    kinds={"python", "pandas"},
+)
+def raw_matches_df(
+    context: AssetExecutionContext, fpl_api: FplAPI, players: pd.DataFrame
+) -> pd.DataFrame:
+
+    player_match_lst = []
+    for player in players["id"].unique():
+        context.log.info(player)
+
+        payload = fpl_api.get_request(endpoint=f"element-summary/{player}/").json()
+
+        player_matches_df = pd.DataFrame.from_records(payload["history"])
+
+        player_match_lst.append(player_matches_df)
+
+    match_stats_history = pd.concat(player_match_lst)
+
+    return match_stats_history
+
+
+@asset(
+    group_name="MATCH",
+    description="""All match statistics for a given player""",
+    kinds={"python", "pandas"},
+)
+def matches_df(
+    context: AssetExecutionContext,
+    raw_matches_df: pd.DataFrame,
+    fixtures: pd.DataFrame,
+) -> None:
+
+    drop_cols = [
+        "id",
+        "opponent_team",
+        "kickoff_time",
+        "team_h_score",
+        "team_a_score",
+        "round",
+        "modified",
+        "season",
+        "event",
+        "finished",
+        "fixture_type",
+        "extract_dt",
+    ]
+
+    match_stats = (
+        raw_matches_df.drop("kickoff_time", axis=1)
+        .merge(
+            fixtures, how="left", left_on=["fixture"], right_on=["id"], validate="m:1"
+        )
+        .drop(drop_cols, axis=1)
+    )
+
+    match_stats["team_id"] = match_stats.apply(
+        lambda x: x["team_h"] if x["was_home"] else x["team_a"], axis=1
+    )
