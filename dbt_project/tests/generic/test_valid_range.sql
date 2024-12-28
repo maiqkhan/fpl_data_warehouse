567b83e44d835e50242c25cb6842cc6d7c081662
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-28 17:17:22 -0500

    add valid_range generic test

---
+++ dbt_project/tests/generic/test_valid_range.sql
@@ -0,0 +1,7 @@
+{% test valid_range(model, column_name, min_value, max_value) %}
+
+select *
+from {{ model }}
+where {{ column_name }} < {{ min_value }} or {{ column_name }} > {{ max_value }}
+
+{% endtest %}
