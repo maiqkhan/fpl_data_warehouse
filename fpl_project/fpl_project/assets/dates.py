43d5e3d1d85262fe4af2085db8a018eb3b999e0f
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-28 17:31:45 -0500

    add date array asset

--- fpl_project/fpl_project/assets/dates.py
+++ fpl_project/fpl_project/assets/dates.py
@@ -12,6 +12,52 @@ from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 
 
+def generate_date_fields_array(dt_lst: List[datetime]) -> List[Dict]:
+
+    dt_dict_array = []
+
+    month_dict = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+    }
+
+    day_dict = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday",
+    }
+
+    for dt in dt_lst:
+        date_dict = {
+            "date_key": int(datetime.strftime(dt, "%Y%m%d")),
+            "date_id": dt,
+            "year": dt.year,
+            "month_num": dt.month,
+            "month_name": month_dict.get(dt.month),
+            "day_of_month": dt.day,
+            "day_of_week": dt.weekday() + 1,
+            "day_name": day_dict.get(dt.weekday() + 1),
+        }
+
+        dt_dict_array.append(date_dict)
+
+    return dt_dict_array
+
+
 @asset(
     group_name="DATE",
     description="""Date Table ranging 5 years from the first fixture of the season""",
@@ -46,26 +92,13 @@ def dates_df(context: AssetExecutionContext, first_fixture_date: date) -> pd.Dat
 
     five_years_later = first_fixture_date + relativedelta(years=5)
 
-    date_list = []
-    current_date = first_fixture_date
+    five_year_dt_lst = [
+        first_fixture_date + relativedelta(days=x)
+        for x in range((five_years_later - first_fixture_date).days)
+    ]
 
-    while current_date <= five_years_later:
+    five_year_dt_dict = generate_date_fields_array(five_year_dt_lst)
 
-        date_dict = {
-            "date_key": int(datetime.strftime(current_date, "%Y%m%d")),
-            "date_id": current_date,
-            "year": current_date.year,
-            "month_num": current_date.month,
-            "month_name": month_dict.get(current_date.month),
-            "day_of_month": current_date.day,
-            "day_of_week": current_date.weekday() + 1,
-            "day_name": day_dict.get(current_date.weekday() + 1),
-        }
-
-        date_list.append(date_dict)
-
-        current_date += relativedelta(days=1)
-
-    date_df = pd.DataFrame.from_records(date_list)
+    date_df = pd.DataFrame.from_records(five_year_dt_dict)
 
     return date_df
