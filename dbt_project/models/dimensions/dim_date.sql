d8ed0dc7a3f39db7de821253575c1f38e66b8fea
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-28 21:16:50 -0500

    modify dim_date model to incremental

--- dbt_project/models/dimensions/dim_date.sql
+++ dbt_project/models/dimensions/dim_date.sql
@@ -1,8 +1,18 @@
 
-{{ config(materialized='table') }}
+{{  config(
+        materialized='incremental'
+    ) }}
 
 with staging as (
     select * from {{ source('stg', 'dates')}}
 )
 
-select * from staging
+select * from 
+
+staging
+
+{% if is_incremental() %}
+
+where date_key >= (select coalesce(max(date_key),19000101) from {{ this }} )
+
+{% endif %}
