af3e4e7a78614a54dbd1f400681485cece472eb0
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-31 17:43:39 -0500

    add initial dim_player table asset

--- fpl_project/fpl_project/assets/players.py
+++ fpl_project/fpl_project/assets/players.py
@@ -7,8 +7,7 @@ from dagster import (
 )
 import pandas as pd
 from fpl_project.fpl_project.resources.postgres import PostgresResource
-from fpl_project.fpl_project.resources.fpl_api import FplAPI
-from fpl_project.fpl_project.assets.raw import raw_bootstrap
+from fpl_project.fpl_project.assets.models import dim_player, Base
 from typing import Dict, List
 from datetime import datetime as dt, timedelta as tmdelta
 
@@ -93,12 +92,13 @@ def unique_player_check(
 @asset(
     group_name="INITIAL_LOAD",
     description="Apply Type 2 Slowly Changing Dimension logic to historical player data",
+    kinds={"python", "pandas"},
 )
 def player_scd_type_2_df(
     context: AssetExecutionContext,
     matches_df: pd.DataFrame,
     players: pd.DataFrame,
-) -> None:
+) -> pd.DataFrame:
 
     player_hist_df = matches_df.merge(
         players, how="inner", left_on="element", right_on="player_id", validate="m:1"
@@ -124,7 +124,7 @@ def player_scd_type_2_df(
         )
 
     player_hist_df["new_record"] = (
-        player_hist_df["id_change"]
+        player_hist_df["player_id_change"]
         | player_hist_df["value_change"]
         | player_hist_df["team_id_change"]
     )
@@ -142,26 +142,26 @@ def player_scd_type_2_df(
             position=("position", "first"),
             price=("value", "first"),
             team_id=("team_id", "first"),
-            effective_date=("kickoff_time", "first"),
+            effective_dt=("kickoff_time", "first"),
         )
         .reset_index(drop=True)
     )
 
-    scd_player_history["effective_date"] = scd_player_history["effective_date"].apply(
+    scd_player_history["effective_dt"] = scd_player_history["effective_dt"].apply(
         lambda x: x.date()
     )
 
     scd_player_history["is_last_record"] = (
-        scd_player_history.groupby("player_id")["effective_date"].transform("max")
-        == scd_player_history["effective_date"]
+        scd_player_history.groupby("player_id")["effective_dt"].transform("max")
+        == scd_player_history["effective_dt"]
     )
 
-    scd_player_history["next_effective_date"] = scd_player_history.groupby("player_id")[
-        "effective_date"
+    scd_player_history["next_effective_dt"] = scd_player_history.groupby("player_id")[
+        "effective_dt"
     ].shift(-1)
 
-    scd_player_history["expiry_date"] = scd_player_history.apply(
-        lambda x: generate_expiry_date(x["next_effective_date"], x["is_last_record"]),
+    scd_player_history["expiry_dt"] = scd_player_history.apply(
+        lambda x: generate_expiry_date(x["next_effective_dt"], x["is_last_record"]),
         axis=1,
     )
 
@@ -170,176 +170,213 @@ def player_scd_type_2_df(
     )
 
     scd_player_history = scd_player_history.drop(
-        ["is_last_record", "next_effective_date"], axis=1
+        ["is_last_record", "next_effective_dt"], axis=1
     )
 
-    context.log.info(scd_player_history.query("player_id == 531"))
+    return scd_player_history
 
 
 @asset(
-    group_name="PLAYER_CHECK",
-    description="Current player data from fpl.dim_player table",
+    group_name="INITIAL_LOAD",
+    description="Apply Type 2 Slowly Changing Dimension logic to historical player data",
+    kinds={"python", "pandas"},
 )
-def db_player_data(fpl_server: PostgresResource) -> pd.DataFrame:
-
-    engine = fpl_server.connect_to_engine()
-
-    with engine.connect() as connection:
-        current_players_df = pd.read_sql(
-            """select player_id
-                    ,d_player.season
-                    ,d_player.first_name
-                    ,d_player.last_name
-                    ,d_player.web_name
-                    ,d_player."position"
-                    ,d_player.price
-                    ,d_team.team_id
-                    ,d_player.effective_dt
-                    ,d_player.expiry_dt
-                    ,d_player.current_ind
-                from fpl.dim_player as d_player 
-                left join fpl.dim_team as d_team on d_player.team_key = d_team.team_key
-                   where d_player.current_ind = True""",
-            connection,
-        )
-
-    return current_players_df
-
-
-@asset(
-    group_name="PLAYER_CHECK",
-    description="""Current player data from FPL api bootstrap-static endpoint""",
-)
-def api_player_data(raw_bootstrap: Dict) -> pd.DataFrame:
-
-    raw_api_player_data = pd.DataFrame.from_records(raw_bootstrap["elements"])
-
-    player_dict = {1: "Goalkeeper", 2: "Defender", 3: "Midfielder", 4: "Forward"}
-
-    # keep only relevant columns from api endpoint payload
-    player_data_df = raw_api_player_data[
-        [
-            "id",
-            "first_name",
-            "second_name",
-            "web_name",
-            "element_type",
-            "now_cost",
-            "team",
-        ]
-    ]
-
-    # remap element type to valid position name
-    player_data_df["position"] = player_data_df["element_type"].apply(player_dict.get)
-
-    # remap price to float value
-    player_data_df["price"] = player_data_df["now_cost"].apply(lambda x: x / 10)
-
-    # Drop element type column
-    player_data_df = player_data_df.drop(["element_type", "now_cost"], axis=1)
-
-    # remap column names to match fpl.dim_player table column names
-    player_data_df = player_data_df.rename(
-        columns={
-            "id": "player_id",
-            "second_name": "last_name",
-            "team": "team_id",
-        }
-    )
-
-    return player_data_df
-
-
-@asset(
-    group_name="PLAYER_CHECK",
-    description="""Players with new values for dimension columns""",
-)
-def existing_players_new_data(
+def initial_dim_player(
     context: AssetExecutionContext,
-    api_player_data: pd.DataFrame,
-    db_player_data: pd.DataFrame,
-) -> pd.DataFrame:
-
-    player_data_comp = db_player_data.merge(
-        api_player_data,
-        how="left",
-        left_on=["player_id"],
-        right_on=["player_id"],
-        suffixes=("_db", "_api"),
-        validate="1:1",
-    )
-
-    existing_player_updates = player_data_comp.query(
-        "first_name_db != first_name_api or last_name_db != last_name_api or web_name_db != web_name_api or price_db != price_api or team_id_db != team_id_api"
-    )
-
-    context.log.info(f"{existing_player_updates}")
-
-    return existing_player_updates
-
-
-@asset(
-    group_name="PLAYER_CHECK",
-    description="""Players that are not in the dim_player table""",
-)
-def new_player_data(
-    context: AssetExecutionContext,
-    api_player_data: pd.DataFrame,
-    db_player_data: pd.DataFrame,
-):
-    new_players_api = api_player_data.merge(
-        db_player_data,
-        how="left",
-        left_on=["player_id"],
-        right_on=["player_id"],
-        suffixes=("_api", "_db"),
-        validate="1:1",
-    )
-
-    new_players = new_players_api.query("team_id_db.isnull()", engine="python")
-
-    context.log.info(f"{new_players}")
-
-    return new_players
-
-
-@asset(
-    group_name="PLAYER_CHECK", description="""Staging table for new player updates"""
-)
-def staging_player_updates(
-    context: AssetExecutionContext,
-    existing_players_new_data: pd.DataFrame,
-    new_player_data: pd.DataFrame,
+    player_scd_type_2_df: pd.DataFrame,
     fpl_server: PostgresResource,
-) -> None:
+):
 
-    stg_tbl_cols = [
-        "player_id",
-        "first_name_api",
-        "last_name_api",
-        "web_name_api",
-        "position_api",
-        "team_id_api",
-        "price_api",
-    ]
+    table_name = dim_player.__tablename__
+    schema_name = dim_player.__table_args__["schema"]
+    table_inst = dim_player.__table__
 
-    player_updates = pd.concat(
-        [existing_players_new_data[stg_tbl_cols], new_player_data[stg_tbl_cols]]
-    ).sort_values(by=["player_id"])
+    player_scd_type_2_df["team_key"] = player_scd_type_2_df.apply(
+        lambda x: int(f"{x['season'][:4]}{x['season'][5:]}{x['team_id']}"), axis=1
+    )
 
-    player_updates.columns = [col.replace("_api", "") for col in player_updates.columns]
+    player_scd_type_2_df = player_scd_type_2_df.drop("team_id", axis=1)
 
-    player_updates["extract_dt"] = dt.today().date() - tmdelta(days=1)
-
-    context.log.info(f"{player_updates}")
+    context.log.info(player_scd_type_2_df.columns)
 
     engine = fpl_server.connect_to_engine()
 
-    with engine.connect() as connection:
-        player_updates.to_sql(
-            name="player_updates",
-            schema="stg",
-            con=connection,
-            index=False,
-            if_exists="append",
-        )
+    Base.metadata.create_all(engine, tables=[table_inst])
+
+    player_scd_type_2_df.sort_values(by=["player_id", "effective_dt"]).to_sql(
+        name=table_name,
+        schema=schema_name,
+        if_exists="append",
+        chunksize=220,
+        index=False,
+        con=engine,
+    )
+
+
+# @asset(
+#     group_name="PLAYER_CHECK",
+#     description="Current player data from fpl.dim_player table",
+# )
+# def db_player_data(fpl_server: PostgresResource) -> pd.DataFrame:
+
+#     engine = fpl_server.connect_to_engine()
+
+#     with engine.connect() as connection:
+#         current_players_df = pd.read_sql(
+#             """select player_id
+#                     ,d_player.season
+#                     ,d_player.first_name
+#                     ,d_player.last_name
+#                     ,d_player.web_name
+#                     ,d_player."position"
+#                     ,d_player.price
+#                     ,d_team.team_id
+#                     ,d_player.effective_dt
+#                     ,d_player.expiry_dt
+#                     ,d_player.current_ind
+#                 from fpl.dim_player as d_player
+#                 left join fpl.dim_team as d_team on d_player.team_key = d_team.team_key
+#                    where d_player.current_ind = True""",
+#             connection,
+#         )
+
+#     return current_players_df
+
+
+# @asset(
+#     group_name="PLAYER_CHECK",
+#     description="""Current player data from FPL api bootstrap-static endpoint""",
+# )
+# def api_player_data(raw_bootstrap: Dict) -> pd.DataFrame:
+
+#     raw_api_player_data = pd.DataFrame.from_records(raw_bootstrap["elements"])
+
+#     player_dict = {1: "Goalkeeper", 2: "Defender", 3: "Midfielder", 4: "Forward"}
+
+#     # keep only relevant columns from api endpoint payload
+#     player_data_df = raw_api_player_data[
+#         [
+#             "id",
+#             "first_name",
+#             "second_name",
+#             "web_name",
+#             "element_type",
+#             "now_cost",
+#             "team",
+#         ]
+#     ]
+
+#     # remap element type to valid position name
+#     player_data_df["position"] = player_data_df["element_type"].apply(player_dict.get)
+
+#     # remap price to float value
+#     player_data_df["price"] = player_data_df["now_cost"].apply(lambda x: x / 10)
+
+#     # Drop element type column
+#     player_data_df = player_data_df.drop(["element_type", "now_cost"], axis=1)
+
+#     # remap column names to match fpl.dim_player table column names
+#     player_data_df = player_data_df.rename(
+#         columns={
+#             "id": "player_id",
+#             "second_name": "last_name",
+#             "team": "team_id",
+#         }
+#     )
+
+#     return player_data_df
+
+
+# @asset(
+#     group_name="PLAYER_CHECK",
+#     description="""Players with new values for dimension columns""",
+# )
+# def existing_players_new_data(
+#     context: AssetExecutionContext,
+#     api_player_data: pd.DataFrame,
+#     db_player_data: pd.DataFrame,
+# ) -> pd.DataFrame:
+
+#     player_data_comp = db_player_data.merge(
+#         api_player_data,
+#         how="left",
+#         left_on=["player_id"],
+#         right_on=["player_id"],
+#         suffixes=("_db", "_api"),
+#         validate="1:1",
+#     )
+
+#     existing_player_updates = player_data_comp.query(
+#         "first_name_db != first_name_api or last_name_db != last_name_api or web_name_db != web_name_api or price_db != price_api or team_id_db != team_id_api"
+#     )
+
+#     context.log.info(f"{existing_player_updates}")
+
+#     return existing_player_updates
+
+
+# @asset(
+#     group_name="PLAYER_CHECK",
+#     description="""Players that are not in the dim_player table""",
+# )
+# def new_player_data(
+#     context: AssetExecutionContext,
+#     api_player_data: pd.DataFrame,
+#     db_player_data: pd.DataFrame,
+# ):
+#     new_players_api = api_player_data.merge(
+#         db_player_data,
+#         how="left",
+#         left_on=["player_id"],
+#         right_on=["player_id"],
+#         suffixes=("_api", "_db"),
+#         validate="1:1",
+#     )
+
+#     new_players = new_players_api.query("team_id_db.isnull()", engine="python")
+
+#     context.log.info(f"{new_players}")
+
+#     return new_players
+
+
+# @asset(
+#     group_name="PLAYER_CHECK", description="""Staging table for new player updates"""
+# )
+# def staging_player_updates(
+#     context: AssetExecutionContext,
+#     existing_players_new_data: pd.DataFrame,
+#     new_player_data: pd.DataFrame,
+#     fpl_server: PostgresResource,
+# ) -> None:
+
+#     stg_tbl_cols = [
+#         "player_id",
+#         "first_name_api",
+#         "last_name_api",
+#         "web_name_api",
+#         "position_api",
+#         "team_id_api",
+#         "price_api",
+#     ]
+
+#     player_updates = pd.concat(
+#         [existing_players_new_data[stg_tbl_cols], new_player_data[stg_tbl_cols]]
+#     ).sort_values(by=["player_id"])
+
+#     player_updates.columns = [col.replace("_api", "") for col in player_updates.columns]
+
+#     player_updates["extract_dt"] = dt.today().date() - tmdelta(days=1)
+
+#     context.log.info(f"{player_updates}")
+
+#     engine = fpl_server.connect_to_engine()
+
+#     with engine.connect() as connection:
+#         player_updates.to_sql(
+#             name="player_updates",
+#             schema="stg",
+#             con=connection,
+#             index=False,
+#             if_exists="append",
+#         )
