d3f1e3c429a09c6d64ab7fa5407759b6635d6cfc
Mustafa Khan <mustafaasif.khan@utoronto.ca>
2024-12-30 23:27:49 -0500

    add fixture dimension dbt model

---
+++ dbt_project/models/dimensions/dim_fixture.sql
@@ -0,0 +1,31 @@
+
+{{  config(
+        materialized='incremental',
+        unique_key='fixture_key',
+        incremental_strategy='merge',
+        merge_exclude_columns = ['fixture_id', 'season'],
+    ) }}
+
+with staging as (
+    select * from {{ source('stg', 'fixtures')}}
+)
+
+select 
+fixture_key
+,fixture_id
+,season
+,"event" as gameweek
+,finished as finished_ind
+,team_h
+,team_a
+,kickoff_time
+,fixture_type
+from 
+
+staging
+
+{% if is_incremental() %}
+
+where fixture_key > (select coalesce(max(fixture_key),1900011) from {{ this }} )
+
+{% endif %}
