0c3524da93af92207bbb905caa353748e6e60eb3
maiqkhan <33614138+maiqkhan@users.noreply.github.com>
2025-03-22 04:43:00 +0000

    reformat raw assets and functions

--- fpl_project/fpl_project/assets/raw.py
+++ fpl_project/fpl_project/assets/raw.py
@@ -14,10 +14,9 @@ from typing import Dict, List
     kinds={"python"},
 )
 def raw_bootstrap(fpl_api: FplAPI) -> Dict:
-
+    """Fetches the game data from the Fantasy Premier League (FPL) API's bootstrap-static endpoint.
+    """
     payload = fpl_api.get_request(endpoint="bootstrap-static/").json()
-
-    # raw_player_data_df = pd.DataFrame.from_records(payload)
 
     return payload
 
@@ -28,6 +27,9 @@ def raw_bootstrap(fpl_api: FplAPI) -> Dict:
     description="""Check that the bootstrap endpoint is returning keys required for donwstream assets""",
 )
 def raw_bootstrap_valid_keys(bootstrap_dict: Dict):
+    """
+    Validates that the bootstrap data returned from the FPL API contains the required keys.
+    """
     valid_key_set = set(["elements", "teams"])
 
     if valid_key_set.issubset(bootstrap_dict.keys()):
@@ -40,23 +42,21 @@ def raw_bootstrap_valid_keys(bootstrap_dict: Dict):
 
 @asset(
     group_name="RAW_DATA",
-    description="""Game data from FPL api bootstrap-static endpoint""",
+    description="""Player data from FPL api bootstrap-static endpoint""",
     kinds={"python"},
 )
 def raw_players(raw_bootstrap: Dict) -> List[Dict]:
-
+    """Extracts the player data from the raw bootstrap data returned by the FPL API."""
     return raw_bootstrap["elements"]
 
 
 @asset(
     group_name="RAW_DATA",
-    description="""Game data from FPL api bootstrap-static endpoint""",
+    description="""Team data from FPL api bootstrap-static endpoint""",
     kinds={"python"},
 )
-def raw_teams(context: AssetExecutionContext, raw_bootstrap: Dict) -> List[Dict]:
-
-    context.log.info(raw_bootstrap["teams"])
-
+def raw_teams(raw_bootstrap: Dict) -> List[Dict]:
+    """Extracts the team data from the raw bootstrap data returned by the FPL API."""
     return raw_bootstrap["teams"]
 
 
@@ -67,6 +67,9 @@ def raw_teams(context: AssetExecutionContext, raw_bootstrap: Dict) -> List[Dict]
     compute_kind="python",
 )
 def raw_teams_20_elements_check(team_lst: List[Dict]):
+    """Verifies that the data returned from the 'bootstrap-static/teams' endpoint contains exactly 20 teams, 
+    as only 20 teams can participate in an EPL season.
+    """
 
     if len(team_lst) != 20:
         return AssetCheckResult(
@@ -83,6 +86,7 @@ def raw_teams_20_elements_check(team_lst: List[Dict]):
     kinds={"python"},
 )
 def raw_fixtures(fpl_api: FplAPI) -> List[Dict]:
+    """Fetches the fixture data for the current Fantasy Premier League season from the API."""
 
     payload = fpl_api.get_request(endpoint="fixtures/").json()
 
@@ -96,6 +100,9 @@ def raw_fixtures(fpl_api: FplAPI) -> List[Dict]:
     compute_kind="python",
 )
 def raw_fixtures_380_elements_check(fixture_lst: List[Dict]):
+    """Checks that the number of fixtures returned from the API equals 380, 
+    which is the exact number of fixtures for an EPL season.
+    """
     if len(fixture_lst) != 380:
         return AssetCheckResult(
             passed=False, metadata={"number_of_fixtures": len(fixture_lst)}
